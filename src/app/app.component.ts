82f9b881a1f11a62211fcdf43c98d13cf5ee98bb
Conor <cfinn14@gmail.comd>
2018-08-12 11:57:57 -0400

    Initial push to master.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import {trigger, style, state, animate, transition} from '@angular/animations';
+import { Hero } from './heroes/hero'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = `A<br>Little<br>Finnegan`;
+  descriptionMain = 'This is a work in progress for the time being.  The main goal is for me to learn more about front end development.'
+  
+  show = false;
+
+  constructor() { }
+
+  get stateName() {
+    return this.show ? 'show' : 'hide'
+  }
+
 }
